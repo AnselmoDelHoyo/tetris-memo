d94a2fa97cf75778b5c7d06029876ff10a322fee
Anselmo1122 <anselmodelhoyo11@gmail.com>
2024-10-02 11:00:27 +0100

    Add random color to the random piece

--- main.js
+++ main.js
@@ -1,198 +1,226 @@
-import './style.css'
+import "./style.css";
 
 // Inicializando Canvas
 
-const canvas = document.getElementById('tetris')
-const context = canvas.getContext('2d')
+const canvas = document.getElementById("tetris");
+const context = canvas.getContext("2d");
 
-const BLOCK_SIZE = 20
-const BOARD_WIDTH = 14
-const BOARD_HEIGHT = 30
+const BLOCK_SIZE = 20;
+const BOARD_WIDTH = 14;
+const BOARD_HEIGHT = 30;
 
-canvas.width = BLOCK_SIZE * BOARD_WIDTH
-canvas.height = BLOCK_SIZE * BOARD_HEIGHT
+canvas.width = BLOCK_SIZE * BOARD_WIDTH;
+canvas.height = BLOCK_SIZE * BOARD_HEIGHT;
 
-context.scale(BLOCK_SIZE, BLOCK_SIZE)
+context.scale(BLOCK_SIZE, BLOCK_SIZE);
+
+// Data
+
+const score = document.getElementById("score");
+const level = document.getElementById("level");
+
+score.textContent = 0;
+level.textContent = 1;
 
 // Board
 
-const board = []
+const board = [];
 
 for (let i = 0; i < 30; i++) {
-  board.push(new Array(14).fill(0))
+  board.push(new Array(14).fill(0));
 }
+
+const COLORS = [
+  "#7C00FE",
+  "#F9E400",
+  "#FFAF00",
+  "#F5004F",
+  "#48CFCB",
+  "#7A1CAC"
+]
 
 const PIECES = [
   [
     [1, 1],
-    [1, 1]
-  ], [
+    [1, 1],
+  ],
+  [
     [0, 1, 0],
-    [1, 1, 1]
-  ], [
+    [1, 1, 1],
+  ],
+  [
     [1, 1, 0],
-    [0, 1, 1]
-  ], [
     [0, 1, 1],
-    [1, 1, 0]
-  ], [
+  ],
+  [
+    [0, 1, 1],
+    [1, 1, 0],
+  ],
+  [
     [1, 0],
     [1, 0],
-    [1, 1]
-  ], [
+    [1, 1],
+  ],
+  [
     [0, 1],
     [0, 1],
-    [1, 1]
-  ], [
-    [1, 1, 1, 1]
-  ]
-]
+    [1, 1],
+  ],
+  [[1, 1, 1, 1]],
+];
 
 const piece = {
   position: { x: 5, y: 0 },
-  shape: getRandomShape()
+  shape: getRandomShape(),
+  color: getRandomColor()
+};
+
+function getRandomShape() {
+  return PIECES[Math.floor(Math.random() * PIECES.length)];
 }
 
-function getRandomShape () {
-  return PIECES[Math.floor(Math.random() * PIECES.length)]
+function getRandomColor() {
+  return COLORS[Math.floor(Math.random() * COLORS.length)];
 }
 
 // Game loop
 // Auto drop
 
-let dropCounter = 0
-let lastTime = 0
+let dropCounter = 0;
+let lastTime = 0;
 
-function update (time = 0) {
-  const deltaTime = time - lastTime
-  lastTime = time
+function update(time = 0) {
+  const deltaTime = time - lastTime;
+  lastTime = time;
 
-  dropCounter += deltaTime
+  dropCounter += deltaTime;
 
   if (dropCounter > 1000) {
-    piece.position.y++
-    dropCounter = 0
+    piece.position.y++;
+    dropCounter = 0;
 
     if (checkCollision()) {
-      piece.position.y--
-      solidifyPiece()
-      removeRows()
+      piece.position.y--;
+      solidifyPiece();
+      removeRows();
     }
   }
 
-  draw()
-  window.requestAnimationFrame(update)
+  draw();
+  window.requestAnimationFrame(update);
 }
 
-function draw () {
-  context.fillStyle = '#000'
-  context.fillRect(0, 0, canvas.width, canvas.height)
+function draw() {
+  context.fillStyle = "#000";
+  context.fillRect(0, 0, canvas.width, canvas.height);
 
   board.forEach((row, y) => {
     row.forEach((value, x) => {
       if (value === 1) {
-        context.fillStyle = 'yellow'
-        context.fillRect(x, y, 1, 1)
+        context.fillStyle = "yellow";
+        context.fillRect(x, y, 1, 1);
       }
-    })
-  })
+    });
+  });
 
   piece.shape.forEach((row, y) => {
     row.forEach((value, x) => {
       if (value) {
-        context.fillStyle = 'red'
-        context.fillRect(x + piece.position.x, y + piece.position.y, 1, 1)
+        context.fillStyle = piece.color;
+        context.fillRect(x + piece.position.x, y + piece.position.y, 1, 1);
       }
-    })
-  })
+    });
+  });
 }
 
-update()
+update();
 
-window.addEventListener('keydown', (e) => {
-  if (e.key === 'ArrowDown') {
-    piece.position.y++
+window.addEventListener("keydown", (e) => {
+  if (e.key === "ArrowDown") {
+    piece.position.y++;
     if (checkCollision()) {
-      piece.position.y--
-      solidifyPiece()
-      removeRows()
+      piece.position.y--;
+      solidifyPiece();
+      removeRows();
     }
   }
-  if (e.key === 'ArrowRight') {
-    piece.position.x++
-    if (checkCollision()) piece.position.x--
+  if (e.key === "ArrowRight") {
+    piece.position.x++;
+    if (checkCollision()) piece.position.x--;
   }
-  if (e.key === 'ArrowLeft') {
-    piece.position.x--
-    if (checkCollision()) piece.position.x++
+  if (e.key === "ArrowLeft") {
+    piece.position.x--;
+    if (checkCollision()) piece.position.x++;
   }
-  if (e.key === 'ArrowUp') {
-    const rotated = []
+  if (e.key === "ArrowUp") {
+    const rotated = [];
 
     for (let i = 0; i < piece.shape[0].length; i++) {
-      const row = []
+      const row = [];
 
       for (let j = piece.shape.length - 1; j >= 0; j--) {
-        row.push(piece.shape[j][i])
+        row.push(piece.shape[j][i]);
       }
 
-      rotated.push(row)
+      rotated.push(row);
     }
 
-    const previousShape = piece.shape
-    piece.shape = rotated
+    const previousShape = piece.shape;
+    piece.shape = rotated;
     if (checkCollision()) {
-      piece.shape = previousShape
+      piece.shape = previousShape;
     }
   }
-})
+});
 
-function checkCollision () {
+function checkCollision() {
   return piece.shape.find((row, y) => {
     return row.find((value, x) => {
       return (
-        value !== 0 &&
-        board[y + piece.position.y]?.[x + piece.position.x] !== 0
-      )
-    })
-  })
+        value !== 0 && board[y + piece.position.y]?.[x + piece.position.x] !== 0
+      );
+    });
+  });
 }
 
-function solidifyPiece () {
+function solidifyPiece() {
   piece.shape.forEach((row, y) => {
     row.forEach((value, x) => {
       if (value === 1) {
-        board[y + piece.position.y][x + piece.position.x] = 1
+        board[y + piece.position.y][x + piece.position.x] = 1;
       }
-    })
-  })
+    });
+  });
 
   // Reset position
-  piece.position.x = Math.floor(BOARD_WIDTH / 2 - 2)
-  piece.position.y = 0
+  piece.position.x = Math.floor(BOARD_WIDTH / 2 - 2);
+  piece.position.y = 0;
 
   // Get random shape
-  piece.shape = getRandomShape()
+  piece.shape = getRandomShape();
+  piece.color = getRandomColor();
 
   // Game Over
   if (checkCollision()) {
-    window.alert('Game Over!! Sorry')
-    board.forEach((row) => row.fill(0))
+    window.alert("Game Over!! Sorry");
+    board.forEach((row) => row.fill(0));
   }
 }
 
-function removeRows () {
-  const rowsToRemove = []
+function removeRows() {
+  const rowsToRemove = [];
 
   board.forEach((row, y) => {
-    if (row.every(value => value === 1)) {
-      rowsToRemove.push(y)
+    if (row.every((value) => value === 1)) {
+      rowsToRemove.push(y);
     }
-  })
+  });
 
-  rowsToRemove.forEach(y => {
-    board.splice(y, 1)
-    const newRow = Array(BOARD_WIDTH).fill(0)
-    board.unshift(newRow)
-  })
+  rowsToRemove.forEach((y) => {
+    score.textContent = parseInt(score.textContent) + 10;
+    if (score > 100) parseInt(level.textContent)++;
+    board.splice(y, 1);
+    const newRow = Array(BOARD_WIDTH).fill(0);
+    board.unshift(newRow);
+  });
 }
